b8880139a2aa8d9a0b59ec92ad29222144dda4b5
JannoTilk <jannotilk@gmail.com>
2022-12-06 13:46:49 +0200

    aoc20_day4 part2

--- aoc20/src/Day4.java
+++ aoc20/src/Day4.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day4 {
     public static void main(String[] args) throws FileNotFoundException {
@@ -18,7 +20,7 @@ public class Day4 {
         System.out.println("elapsed:" + elapsed1);
 
         long timeBefore2 = System.currentTimeMillis();
-//        System.out.println(part2(list));
+        System.out.println(part2(list));
         long timeAfter2 = System.currentTimeMillis();
         long elapsed2 = timeAfter2 - timeBefore2;
         System.out.println("elapsed:" + elapsed2);
@@ -28,28 +30,109 @@ public class Day4 {
         int count = 0;
         Set<String> fields = new HashSet<>();
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).contains("byr")) {
+            String row = list.get(i);
+            if (row.contains("byr:")) {
                 fields.add("byr");
             }
-            if (list.get(i).contains("iyr")) {
+            if (row.contains("iyr:")) {
                 fields.add("iyr");
             }
-            if (list.get(i).contains("eyr")) {
+            if (row.contains("eyr:")) {
                 fields.add("eyr");
             }
-            if (list.get(i).contains("hgt")) {
+            if (row.contains("hgt:")) {
                 fields.add("hgt");
             }
-            if (list.get(i).contains("hcl")) {
+            if (row.contains("hcl:")) {
                 fields.add("hcl");
             }
-            if (list.get(i).contains("ecl")) {
+            if (row.contains("ecl:")) {
                 fields.add("ecl");
             }
-            if (list.get(i).contains("pid")) {
+            if (row.contains("pid:")) {
                 fields.add("pid");
             }
-            if (list.get(i).equals("") || i == list.size() - 1) {
+            if (row.equals("") || i == list.size() - 1) {
+                if (fields.size() == 7) {
+                    count++;
+                }
+                fields = new HashSet<>();
+            }
+        }
+        return count;
+    }
+
+    private static int part2(ArrayList<String> list) {
+        int count = 0;
+        Set<String> fields = new HashSet<>();
+        for (int i = 0; i < list.size(); i++) {
+            String row = list.get(i);
+            if (row.contains("byr:")) {
+                String[] splitted = row.split("byr:");
+                int result = Integer.parseInt(splitted[1].substring(0, 4));
+                if (result >= 1920 && result <= 2002) {
+                    fields.add("byr");
+                }
+            }
+            if (row.contains("iyr:")) {
+                String[] splitted = row.split("iyr:");
+                int result = Integer.parseInt(splitted[1].substring(0, 4));
+                if (result >= 2010 && result <= 2020) {
+                    fields.add("iyr");
+                }
+            }
+            if (row.contains("eyr:")) {
+                String[] splitted = row.split("eyr:");
+                int result = Integer.parseInt(splitted[1].substring(0, 4));
+                if (result >= 2020 && result <= 2030) {
+                    fields.add("eyr");
+                }
+            }
+            if (row.contains("hgt:")) {
+                Pattern cmPattern = Pattern.compile("hgt:\\d{1,3}cm");
+                Matcher cmMatcher = cmPattern.matcher(row);
+
+                Pattern inchPattern = Pattern.compile("hgt:\\d{1,3}in");
+                Matcher inchMatcher = inchPattern.matcher(row);
+
+                if (cmMatcher.find()) {
+                    String[] splitted = cmMatcher.group(0).split("cm");
+                    int result = Integer.parseInt(splitted[0].split("hgt:")[1]);
+                    if (result >= 150 && result <= 193) {
+                        fields.add("hgt");
+                    }
+                } else if (inchMatcher.find()) {
+                    String[] splitted = inchMatcher.group(0).split("in");
+                    int result = Integer.parseInt(splitted[0].split("hgt:")[1]);
+                    if (result >= 59 && result <= 76) {
+                        fields.add("hgt");
+                    }
+                }
+            }
+            if (row.contains("hcl:")) {
+                Pattern r = Pattern.compile("hcl:#[a-f|\\d]{6}");
+                Matcher m = r.matcher(row);
+                if (m.find()) {
+                    fields.add("hcl");
+                }
+            }
+            if (row.contains("ecl:")) {
+                Pattern r = Pattern.compile("ecl:amb|ecl:blu|ecl:brn|ecl:gry|ecl:grn|ecl:hzl|ecl:oth");
+                Matcher m = r.matcher(row);
+                if (m.find()) {
+                    fields.add("ecl");
+                }
+            }
+            if (row.contains("pid:")) {
+                Pattern r = Pattern.compile("pid:\\d+");
+                Matcher m = r.matcher(row);
+                if (m.find()) {
+                    if (m.group(0).split("pid:")[1].length() == 9) {
+                        fields.add("pid");
+                    }
+                }
+            }
+            if (row.equals("") || i == list.size() - 1) {
                 if (fields.size() == 7) {
                     count++;
                 }
