e34dd7b9fb3b89c694de924abdc533d402f6935e
JannoTilk <jannotilk@gmail.com>
2023-02-01 07:05:47 +0200

    aoc22_day10 part2 fix bugs

--- aoc22/src/Day10.java
+++ aoc22/src/Day10.java
@@ -7,22 +7,22 @@ import java.util.Scanner;
 
 public class Day10 {
     public static void main(String[] args) throws FileNotFoundException {
-//        Scanner s = new Scanner(new File("aoc22/src/day10.txt"));
-        Scanner s = new Scanner(new File("aoc22/src/day10_test.txt"));
+        Scanner s = new Scanner(new File("aoc22/src/day10.txt"));
+//        Scanner s = new Scanner(new File("aoc22/src/day10_test.txt"));
         ArrayList<String> list = new ArrayList<>();
         while (s.hasNext()) {
             list.add(s.nextLine());
         }
         s.close();
 
-//        long timeBefore1 = System.currentTimeMillis();
-//        System.out.println(part1(list));
-//        long timeAfter1 = System.currentTimeMillis();
-//        long elapsed1 = timeAfter1 - timeBefore1;
-//        System.out.println("elapsed:" + elapsed1);
+        long timeBefore1 = System.currentTimeMillis();
+        System.out.println(part1(list));
+        long timeAfter1 = System.currentTimeMillis();
+        long elapsed1 = timeAfter1 - timeBefore1;
+        System.out.println("elapsed:" + elapsed1);
 
         long timeBefore2 = System.currentTimeMillis();
-        System.out.println(part2(list));
+        part2(list);
         long timeAfter2 = System.currentTimeMillis();
         long elapsed2 = timeAfter2 - timeBefore2;
         System.out.println("elapsed:" + elapsed2);
@@ -39,9 +39,6 @@ public class Day10 {
             if (instruction.equals("addx")) {
                 cycle++;
                 if (cyclesToCheck.contains(cycle)) {
-                    System.out.println(X);
-                    System.out.println(cycle);
-                    System.out.println(X * cycle);
                     result += X * cycle;
                 }
                 cycle++;
@@ -51,73 +48,66 @@ public class Day10 {
                 cycle++;
             }
             if (cyclesToCheck.contains(cycle)) {
-                System.out.println(X);
-                System.out.println(cycle);
-                System.out.println(X * cycle);
                 result += X * cycle;
             }
-//            System.out.println(input.get(i));
         }
         return result;
     }
 
-    private static boolean part2(List<String> input) {
-        List<Integer> cyclesToCheck = new ArrayList<>(Arrays.asList(40, 80, 120, 160, 200, 240));
+    private static void part2(List<String> input) {
+        int widthCRT = 40;
         List<StringBuilder> sbs = new ArrayList<>();
         int X = 1;
         int cycle = -1;
         StringBuilder sb = new StringBuilder();
         for (String s : input) {
-//            if (cyclesToCheck.contains(cycle + 1)) {
-//                sbs.add(sb);
-//                cycle = -1;
-//                sb = new StringBuilder();
-//            }
             String instruction = s.split(" ")[0];
             if (instruction.equals("addx")) {
+                if (widthCRT == cycle + 1) {
+                    sbs.add(sb);
+                    cycle = -1;
+                    sb = new StringBuilder();
+                }
+                cycle++;
 
-                cycle++;
-                if (cyclesToCheck.contains(cycle + 1)) {
+                generateCRTValue(X, cycle, sb);
+                if (widthCRT == cycle + 1) {
                     sbs.add(sb);
                     cycle = -1;
                     sb = new StringBuilder();
                 }
-                if (cycle >= X - 1 && cycle <= X + 1) {
-                    sb.append("#");
-                } else {
-                    sb.append(".");
-                }
                 cycle++;
-                if (cyclesToCheck.contains(cycle + 1)) {
-                    sbs.add(sb);
-                    cycle = -1;
-                    sb = new StringBuilder();
-                }
-                if (cycle >= X - 1 && cycle <= X + 1) {
-                    sb.append("#");
-                } else {
-                    sb.append(".");
-                }
+
+                generateCRTValue(X, cycle, sb);
+
                 X += Integer.parseInt(s.split(" ")[1]);
             }
 
             if (instruction.equals("noop")) {
-                cycle++;
-                if (cyclesToCheck.contains(cycle + 1)) {
+                if (widthCRT == cycle + 1) {
                     sbs.add(sb);
                     cycle = -1;
                     sb = new StringBuilder();
                 }
-                if (cycle >= X - 1 && cycle <= X + 1) {
-                    sb.append("#");
-                } else {
-                    sb.append(".");
-                }
+                cycle++;
+
+                generateCRTValue(X, cycle, sb);
+
+            }
+            if (widthCRT == cycle + 1 && sbs.size() == 5) {
+                sbs.add(sb);
             }
         }
         for (StringBuilder sb_local : sbs) {
             System.out.println(sb_local);
         }
-        return false;
+    }
+
+    private static void generateCRTValue(int X, int cycle, StringBuilder sb) {
+        if (cycle >= X - 1 && cycle <= X + 1) {
+            sb.append("#");
+        } else {
+            sb.append(".");
+        }
     }
 }
