deef16c8539e9f761763f7596cec94aca647070d
Janno Tilk <jatilk@taltech.ee>
2023-12-02 13:20:19 +0200

    aoc23 day2 clean

--- aoc23/day2.py
+++ aoc23/day2.py
@@ -2,28 +2,28 @@ import datetime
 import re
 
 start = datetime.datetime.now()
-input = []
+_input = []
 _sum = 0
 _sum2 = 0
-red_max = 12
-green_max = 13
-blue_max = 14
+red_total = 12
+green_total = 13
+blue_total = 14
 
 with open("day2.txt", 'r') as f:
 # with open("day2_test.txt", 'r') as f:
     for line in f.readlines():
-        input.append(line.replace("\n", ""))
+        _input.append(line.replace("\n", ""))
 
-for i in range(0, len(input)):
-    blue = re.findall("\\d+ blue", input[i])
-    red = re.findall("\\d+ red", input[i])
-    green = re.findall("\\d+ green", input[i])
-    blue_count = max(int(item.split()[0]) for item in blue)
-    red_count = max(int(item.split()[0]) for item in red)
-    green_count = max(int(item.split()[0]) for item in green)
-    if blue_count <= blue_max and red_count <= red_max and green_count <= green_max:
+for i in range(0, len(_input)):
+    blue = re.findall("\\d+ blue", _input[i])
+    red = re.findall("\\d+ red", _input[i])
+    green = re.findall("\\d+ green", _input[i])
+    blue_max = max(int(item.split()[0]) for item in blue)
+    red_max = max(int(item.split()[0]) for item in red)
+    green_max = max(int(item.split()[0]) for item in green)
+    if blue_max <= blue_total and red_max <= red_total and green_max <= green_total:
         _sum += i + 1
-    _sum2 += (blue_count * red_count * green_count)
+    _sum2 += (blue_max * red_max * green_max)
 
 print("Part 1: ", _sum)
 print("Part 2: ", _sum2)
