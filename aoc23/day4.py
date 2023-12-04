790fc4f195eb6eb8c6f2c4727f89b345be90ed52
Janno Tilk <jatilk@taltech.ee>
2023-12-04 10:01:18 +0200

    aoc23 day4 clean3

--- aoc23/day4.py
+++ aoc23/day4.py
@@ -5,8 +5,9 @@ with open("day4.txt", 'r') as f:
     for line in f.readlines():
         _input.append(line.replace("\n", ""))
 
-def get_winning_numbers(card_index):
+def get_winning_numbers(card_number):
     card_winning_numbers = 0
+    card_index = card_number - 1
     winning = _input[card_index].split(": ")[1].split(" | ")[0].replace("  ", " ").split(" ")
     actual = _input[card_index].split(": ")[1].split(" | ")[1].replace("  ", " ").split(" ")
     for j in range(0, len(winning)):
@@ -16,26 +17,24 @@ def get_winning_numbers(card_index):
 
 def part1():
     _sum = 0
-    for card_index in range(0, len(_input)):
-        card_winning_numbers = get_winning_numbers(card_index)
+    for card_number in range(1, len(_input) + 1):
+        card_winning_numbers = get_winning_numbers(card_number)
         if card_winning_numbers > 0:
             _sum += 2 ** (card_winning_numbers - 1)
     return _sum
 
 def part2():
     _dict = {}
-    for card_index in range(0, len(_input)):
-        card_winning_numbers = get_winning_numbers(card_index)
+    for card_number in range(1, len(_input) + 1):
+        card_winning_numbers = get_winning_numbers(card_number)
 
-        if card_index + 1 in _dict:
-            _dict[card_index + 1] += 1
-        else:
-            _dict.setdefault(card_index + 1, 1)
+        _dict.setdefault(card_number, 0)
+        _dict[card_number] += 1
 
         for k in range(0, card_winning_numbers):
-            if card_index + 1 + k < len(_input) + 1:
-                _dict.setdefault(card_index + 2 + k, 0)
-                _dict[card_index + 2 + k] += 1 if card_index == 0 else _dict[card_index + 1]
+            if card_number + k < len(_input):
+                _dict.setdefault(card_number + 1 + k, 0)
+                _dict[card_number + 1 + k] += _dict[card_number]
     return sum(_dict.values())
 
 print("Part 1: ", part1())
