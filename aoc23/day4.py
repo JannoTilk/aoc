a7b2d73e18f5543d6c9bdd1dbcd94da2af5c7fd0
Janno Tilk <jatilk@taltech.ee>
2023-12-04 09:45:19 +0200

    aoc23 day4 clean2

--- aoc23/day4.py
+++ aoc23/day4.py
@@ -1,40 +1,42 @@
 _input = []
-_sum = 0
-_dict = {}
 
 with open("day4.txt", 'r') as f:
 # with open("day4_test.txt", 'r') as f:
     for line in f.readlines():
         _input.append(line.replace("\n", ""))
 
-def get_winning_cards():
-    global card_winning_numbers
+def get_winning_numbers(card_index):
     card_winning_numbers = 0
-    winning = _input[i].split(": ")[1].split(" | ")[0].replace("  ", " ").split(" ")
-    actual = _input[i].split(": ")[1].split(" | ")[1].replace("  ", " ").split(" ")
+    winning = _input[card_index].split(": ")[1].split(" | ")[0].replace("  ", " ").split(" ")
+    actual = _input[card_index].split(": ")[1].split(" | ")[1].replace("  ", " ").split(" ")
     for j in range(0, len(winning)):
         if winning[j] in actual:
             card_winning_numbers += 1
+    return card_winning_numbers
 
-#part1
-for i in range(0, len(_input)):
-    get_winning_cards()
-    if card_winning_numbers > 0:
-        _sum += 2 ** (card_winning_numbers - 1)
+def part1():
+    _sum = 0
+    for card_index in range(0, len(_input)):
+        card_winning_numbers = get_winning_numbers(card_index)
+        if card_winning_numbers > 0:
+            _sum += 2 ** (card_winning_numbers - 1)
+    return _sum
 
-#part2
-for i in range(0, len(_input)):
-    get_winning_cards()
+def part2():
+    _dict = {}
+    for card_index in range(0, len(_input)):
+        card_winning_numbers = get_winning_numbers(card_index)
 
-    if i + 1 in _dict:
-        _dict[i + 1] += 1
-    else:
-        _dict.setdefault(i + 1, 1)
+        if card_index + 1 in _dict:
+            _dict[card_index + 1] += 1
+        else:
+            _dict.setdefault(card_index + 1, 1)
 
-    for k in range(0, card_winning_numbers):
-        if i + 1 + k < len(_input) + 1:
-            _dict.setdefault(i + 2 + k, 0)
-            _dict[i + 2 + k] += 1 if i == 0 else _dict[i+1]
+        for k in range(0, card_winning_numbers):
+            if card_index + 1 + k < len(_input) + 1:
+                _dict.setdefault(card_index + 2 + k, 0)
+                _dict[card_index + 2 + k] += 1 if card_index == 0 else _dict[card_index + 1]
+    return sum(_dict.values())
 
-print("Part 1: ", _sum)
-print("Part 2: ", sum(_dict.values()))
+print("Part 1: ", part1())
+print("Part 2: ", part2())
